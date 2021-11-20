0c2b7369020ce7e4aca6d9d6ca793afef832ff75
Bruno Irokawa Fernandes <bruno.fernandes@fiap.com.br>
2021-11-20 10:41:25 -0300

    refactor(Header): extract to separate component

---
+++ src/components/Content/Header/index.tsx
@@ -0,0 +1,25 @@
+import { BuyAHouseIcon } from 'assets';
+import React from 'react';
+import { Col, Row } from 'styles/layout';
+import { colors } from 'styles/variables';
+import { Text } from 'styles/typography';
+
+function Header(): JSX.Element {
+  return (
+    <Col span={9}>
+      <Row justify="space-between">
+        <BuyAHouseIcon />
+        <Col span={8} margin="0 0 0 16px" justify="space-evenly">
+          <Text size="large" bold>
+            Buy a house
+          </Text>
+          <Text size="medium" color={colors.BLUE_GRAY}>
+            Saving goal
+          </Text>
+        </Col>
+      </Row>
+    </Col>
+  );
+}
+
+export default Header;
