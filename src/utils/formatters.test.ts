0d707d0b9a74e5c9048d1558d543341818c3ba52
Bruno Irokawa Fernandes <bruno.fernandes@fiap.com.br>
2021-11-20 13:43:43 -0300

    refactor: change masks to formatters

---
+++ src/utils/formatters.test.ts
@@ -0,0 +1,42 @@
+import { NOT_NUMBER_PARSEABLE } from './constants';
+import { formatCurrency, formatDecimal } from './formatters';
+
+describe('formatters/formatCurrency', () => {
+  test('Formats the value correctly', () => {
+    const result = formatCurrency('1000');
+
+    expect(result).toEqual('1,000.00');
+  });
+
+  test('Formats the value correctly with decimal places', () => {
+    const result = formatCurrency('1000.500');
+
+    expect(result).toEqual('1,000.50');
+  });
+
+  test('Throws an error if provided value is not parseable to Number', () => {
+    const result = () => formatCurrency('a');
+
+    expect(result).toThrow(NOT_NUMBER_PARSEABLE);
+  });
+});
+
+describe('formatters/formatDecimal', () => {
+  test('Does not add unnecessary decimal places', () => {
+    const result = formatDecimal(1000);
+
+    expect(result).toEqual('1,000');
+  });
+
+  test('Formats to two decimal places', () => {
+    const result = formatDecimal(1000.1);
+
+    expect(result).toEqual('1,000.10');
+  });
+
+  test('Throws an error if provided value is not parseable to Number', () => {
+    const result = () => formatDecimal('a');
+
+    expect(result).toThrow(NOT_NUMBER_PARSEABLE);
+  });
+});
