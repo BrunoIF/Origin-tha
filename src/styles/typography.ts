b8e434159202d0040778b06978460c499549d1e1
Bruno Irokawa Fernandes <bruno.fernandes@fiap.com.br>
2021-11-20 02:11:35 -0300

    feat: create Text styles

--- src/styles/typography.ts
+++ src/styles/typography.ts
@@ -17,3 +17,27 @@ export const Title = styled.h1<TitleProps>`
     font-weight: 600;
   }
 `;
+
+type TextSizes = 'small' | 'medium' | 'large';
+interface TextProps {
+  size?: TextSizes;
+  color?: string;
+  bold?: boolean;
+}
+
+const TEXT_FONT_SIZES: { [key in TextSizes]: string } = {
+  small: '12px',
+  medium: '14px',
+  large: '20px',
+};
+
+export const Text = styled.p<TextProps>`
+  font-size: ${(props) => TEXT_FONT_SIZES[props.size as TextSizes]};
+  color: ${(props) => props.color};
+  font-weight: ${(props) => (props.bold ? '600' : '400')};
+`;
+
+Text.defaultProps = {
+  size: 'medium',
+  color: colors.DARK,
+};
