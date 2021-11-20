53b92be1d54c627c2abac169df857b2f435ab3f3
Bruno Irokawa Fernandes <bruno.fernandes@fiap.com.br>
2021-11-20 14:32:02 -0300

    feat(Input): apply mask on value

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -2,38 +2,57 @@ import React, { useRef, useState } from 'react';
 import * as S from './styles';
 
 interface Props {
-  value: string;
+  value?: string;
   placeholder?: string;
   onChange?(value: string): void;
   preffix?: React.ReactNode;
   type?: 'text' | 'number';
+  mask?(value: string): string;
 }
 
 function Input({
-  value,
+  value = '',
   onChange,
   preffix,
   placeholder,
   type = 'text',
+  mask,
   ...props
 }: Props): JSX.Element {
-  const [inputValue, setInputValue] = useState<string>(value);
+  const [inputValue, setInputValue] = useState<string>(mask?.(value) ?? value);
   const inputRef = useRef<HTMLDivElement>();
 
-  const handleChange = (val: string) => {
-    setInputValue(val);
-    onChange?.(inputValue);
+  const handleChange = (el: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = el;
+
+    const areAllNumberRegex = /^([\d,.]+)$/;
+    if (
+      type === 'number' &&
+      !areAllNumberRegex.test(value) &&
+      value.length !== 0
+    ) {
+      return;
+    }
+
+    const formattedValue = mask?.(value) ?? value;
+    setInputValue(formattedValue);
+    onChange?.(value);
   };
 
   return (
-    <S.InputContainer onClick={() => inputRef.current?.focus()}>
+    <S.InputContainer
+      onClick={() => inputRef.current?.focus()}
+      data-testid="inputContainer"
+    >
       {preffix}
       <S.StyledInput
         value={inputValue}
-        onChange={(el) => handleChange(el.target.value)}
+        onChange={handleChange}
         placeholder={placeholder}
         ref={inputRef as any}
-        type={type}
+        data-testid="input"
         {...props}
       ></S.StyledInput>
     </S.InputContainer>
