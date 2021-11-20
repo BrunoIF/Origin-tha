18aad64d033426f9a787181caa05cbe389f357e1
Bruno Irokawa Fernandes <bruno.fernandes@fiap.com.br>
2021-11-20 19:49:50 -0300

    feat(Text): add font type and align props

--- src/styles/typography.ts
+++ src/styles/typography.ts
@@ -9,8 +9,8 @@ export const Title = styled.h1<TitleProps>`
   font-family: 'Work Sans', sans-serif;
   font-size: 18px;
   font-weight: 400;
-  color: ${colors.BLUE};
   text-align: center;
+  color: ${colors.PRIMARY};
   margin: ${(props) => props.margin};
 
   span {
@@ -18,18 +18,29 @@ export const Title = styled.h1<TitleProps>`
   }
 `;
 
-type TextSizes = 'small' | 'medium' | 'large';
+type TextSizes = 'small' | 'medium' | 'large' | 'xlarge';
+
+type FontTypes = 'primary' | 'secondary';
+
 interface TextProps {
   size?: TextSizes;
   color?: string;
   bold?: boolean;
   margin?: string;
+  font?: FontTypes;
+  align?: 'left' | 'center' | 'right';
 }
 
 const TEXT_FONT_SIZES: { [key in TextSizes]: string } = {
   small: '12px',
   medium: '14px',
   large: '20px',
+  xlarge: '24px',
+};
+
+const FONT_STYLES: { [key in FontTypes]: { [prop: string]: string } } = {
+  primary: { 'font-family': 'Work Sans, sans-serif' },
+  secondary: { 'font-family': 'Rubik, sans-serif', 'font-weight': '500' },
 };
 
 export const Text = styled.p<TextProps>`
@@ -37,9 +48,18 @@ export const Text = styled.p<TextProps>`
   color: ${(props) => props.color};
   font-weight: ${(props) => (props.bold ? '600' : '400')};
   margin: ${(props) => props.margin};
+  text-align: ${(props) => props.align};
+
+  ${(props) => ({ ...FONT_STYLES[props.font as FontTypes] })};
+
+  span.bold {
+    font-weight: 600;
+  }
 `;
 
 Text.defaultProps = {
   size: 'medium',
   color: colors.DARK,
+  font: 'primary',
+  align: 'left',
 };
