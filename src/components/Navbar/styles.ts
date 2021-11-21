a089dd78b5e957b5fb87bdf16f492a6e842ca2a5
Bruno Irokawa Fernandes <bruno.fernandes@fiap.com.br>
2021-11-21 17:59:21 -0300

    feat(Navbar): add desktop styles

--- src/components/Navbar/styles.ts
+++ src/components/Navbar/styles.ts
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 import { colors } from 'styles/variables';
 import { OriginLogo as OriginLogoSVG } from 'assets';
+import { MOBILE_BREAKPOINT } from 'utils/constants';
 
 export const Navbar = styled.div`
   height: 56px;
@@ -10,9 +11,19 @@ export const Navbar = styled.div`
   align-items: center;
   padding: 16px;
   box-sizing: border-box;
+
+  @media screen and (min-width: ${MOBILE_BREAKPOINT}px) {
+    height: 80px;
+    padding: 24px 56px;
+  }
 `;
 
 export const OriginLogo = styled(OriginLogoSVG)`
   width: 75px;
   height: 24px;
+
+  @media screen and (min-width: ${MOBILE_BREAKPOINT}px) {
+    height: 32px;
+    width: 100px;
+  }
 `;
