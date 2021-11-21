5584abe9132114df4a7eaf73b22bd737adbb094c
Bruno Irokawa Fernandes <bruno.fernandes@fiap.com.br>
2021-11-20 22:59:34 -0300

    feat(SavingsStore): implement actions and computed observables

--- src/stores/SavingsStore.ts
+++ src/stores/SavingsStore.ts
@@ -1,46 +1,61 @@
 import { computed, makeObservable, observable } from 'mobx';
+import { formatDecimal } from 'utils/formatters';
 import { RootStore } from './RootStore';
 
 export class SavingsStore {
   protected rootStore;
 
-  public month: number;
-  public year: number;
-  public totalAmount = 0;
+  public goalMonth: number;
+  public goalYear: number;
+  public goalAmount = 0;
 
   constructor(rootStore: RootStore) {
     makeObservable(this, {
-      month: observable,
-      year: observable,
-      totalAmount: observable,
-      monthlySaving: computed,
+      goalMonth: observable,
+      goalYear: observable,
+      goalAmount: observable,
+      monthlyAmount: computed,
       totalMonths: computed,
     });
 
     this.rootStore = rootStore;
 
     const date = new Date();
-    this.month = date.getMonth();
-    this.year = date.getFullYear();
+    this.goalMonth = date.getMonth();
+    this.goalYear = date.getFullYear();
   }
 
   public get totalMonths(): number {
-    return 1;
+    const currentDate = new Date();
+    const currentMonth = currentDate.getMonth();
+    const currentYear = currentDate.getFullYear();
+
+    const additionalYears = this.goalYear - currentYear;
+
+    const totalMonths = additionalYears * 12 + (11 - currentMonth);
+
+    return totalMonths;
   }
 
-  public get monthlySaving(): number {
-    return 1;
+  public get monthlyAmount(): number {
+    if (!this.goalAmount || !this.totalMonths) return 0;
+
+    const monthlyAmount = formatDecimal(
+      this.goalAmount / (this.totalMonths || 1)
+    );
+
+    return Number(monthlyAmount);
   }
 
-  public setMonth(month: number): void {
-    this.month = month;
+  public setGoalMonth(month: number): void {
+    this.goalMonth = month;
   }
 
-  public setYear(year: number): void {
-    this.year = year;
+  public setGoalYear(year: number): void {
+    this.goalYear = year;
   }
 
-  public setTotalAmount(value: number): void {
-    this.totalAmount = value;
+  public setGoalAmount(value: number): void {
+    this.goalAmount = value;
   }
 }
