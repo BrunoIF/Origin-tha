d949fca744b7f86a51760b986c06f3a3f70c913d
Bruno Irokawa Fernandes <bruno.fernandes@fiap.com.br>
2021-11-21 19:49:50 -0300

    feat(LabelContainer): improve elements spacing for desktop

--- src/components/LabelContainer/styles.ts
+++ src/components/LabelContainer/styles.ts
@@ -1,8 +1,15 @@
 import styled from 'styled-components';
+import { MOBILE_BREAKPOINT } from 'utils/constants';
 
 export const Container = styled.div`
   width: 100%;
   > * + * {
     margin-top: 4px;
+  }
+
+  @media screen and (min-width: ${MOBILE_BREAKPOINT}px) {
+    > * + * {
+      margin-top: 6px;
+    }
   }
 `;
