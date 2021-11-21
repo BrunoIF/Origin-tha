6a994d0f1bab7f419c16b8f201aa39b3fc73c02c
Bruno Irokawa Fernandes <bruno.fernandes@fiap.com.br>
2021-11-20 23:47:34 -0300

    feat(Content): add SavingsSummary and confirmation Button

--- src/components/Content/index.tsx
+++ src/components/Content/index.tsx
@@ -1,6 +1,8 @@
 import Amount from 'components/Amount';
+import Button from 'components/Button';
 import LabelContainer from 'components/LabelContainer';
 import ReachDate from 'components/ReachDate';
+import SavingsSummary from 'components/SavingsSummary';
 import React from 'react';
 import { Row } from 'styles/layout';
 import Header from './Header';
@@ -19,6 +21,12 @@ function Content(): JSX.Element {
         <LabelContainer label="Reach goal by">
           <ReachDate />
         </LabelContainer>
+      </Row>
+      <Row margin="24px 0 0 0">
+        <SavingsSummary />
+      </Row>
+      <Row margin="32px 0 0">
+        <Button>Confirm</Button>
       </Row>
     </S.Card>
   );
