61dd8f305df92b0de4a43acc676042b55d333340
Bruno Irokawa Fernandes <bruno.fernandes@fiap.com.br>
2021-11-21 18:05:33 -0300

    feat(Header): update grid layout

--- src/components/Content/Header/index.tsx
+++ src/components/Content/Header/index.tsx
@@ -6,19 +6,17 @@ import { Text } from 'styles/typography';
 
 function Header(): JSX.Element {
   return (
-    <Col span={9}>
-      <Row justify="space-between">
-        <BuyAHouseIcon />
-        <Col span={8} margin="0 0 0 16px" justify="space-evenly">
-          <Text size="large" bold>
-            Buy a house
-          </Text>
-          <Text size="medium" color={colors.BLUE_GRAY_400}>
-            Saving goal
-          </Text>
-        </Col>
-      </Row>
-    </Col>
+    <Row>
+      <BuyAHouseIcon />
+      <Col span={8} margin="0 0 0 16px" justify="space-evenly">
+        <Text size="large" bold>
+          Buy a house
+        </Text>
+        <Text size="medium" color={colors.BLUE_GRAY_400}>
+          Saving goal
+        </Text>
+      </Col>
+    </Row>
   );
 }
 
