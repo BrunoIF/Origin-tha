acbe575410790efe302806de8c70fe9cde714123
Bruno Irokawa Fernandes <bruno.fernandes@fiap.com.br>
2021-11-20 23:03:28 -0300

    feat(SavingsSummary): implement store values

--- src/components/SavingsSummary/index.tsx
+++ src/components/SavingsSummary/index.tsx
@@ -1,26 +1,38 @@
+import useStores from 'hooks/useStores';
+import { observer } from 'mobx-react';
 import React from 'react';
 import { Row } from 'styles/layout';
 import { Text } from 'styles/typography';
 import { colors } from 'styles/variables';
+import { MONTHS } from 'utils/constants';
 import * as S from './styles';
 
 function SavingsSummary(): JSX.Element {
+  const { savingsStore } = useStores();
+
   return (
     <S.SummaryContainer>
       <S.SummaryContent>
         <Row justify="space-between" align="center">
           <Text size="large">Monthly amount</Text>
           <Text size="xlarge" color={colors.SECONDARY} font="secondary">
-            $520.83
+            ${savingsStore.monthlyAmount}
           </Text>
         </Row>
       </S.SummaryContent>
       <S.SummaryContent bgColor={colors.BLUE_GRAY_10}>
         <Row>
           <Text align="center" size="small">
-            You’re planning <span className="bold">48 monthly deposits</span> to
-            reach your <span className="bold">$25,000</span> goal by{' '}
-            <span className="bold">October 2020</span>.
+            You’re planning{' '}
+            <span className="bold">
+              {savingsStore.totalMonths} monthly deposits
+            </span>{' '}
+            to reach your{' '}
+            <span className="bold">${savingsStore.goalAmount}</span> goal by{' '}
+            <span className="bold">
+              {MONTHS[savingsStore.goalMonth]} {savingsStore.goalYear}
+            </span>
+            .
           </Text>
         </Row>
       </S.SummaryContent>
@@ -28,4 +40,4 @@ function SavingsSummary(): JSX.Element {
   );
 }
 
-export default SavingsSummary;
+export default observer(SavingsSummary);
