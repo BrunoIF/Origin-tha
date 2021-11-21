4bcebd7cbd72f1225e05630e7a2966633f458e15
Bruno Irokawa Fernandes <bruno.fernandes@fiap.com.br>
2021-11-21 01:00:06 -0300

    feat(Amount): implement savingsStore action

--- src/components/Amount/index.tsx
+++ src/components/Amount/index.tsx
@@ -1,15 +1,25 @@
 import { CurrencyIcon } from 'assets';
 import Input from 'components/Input';
+import useStores from 'hooks/useStores';
 import React from 'react';
 import { maskNumber } from 'utils/masks';
+import { sanitizeNumber } from 'utils/utils';
 
 function Amount(): JSX.Element {
+  const { savingsStore } = useStores();
+
+  const handleChange = (value: string) => {
+    const sanitized = sanitizeNumber(value);
+    savingsStore.setGoalAmount(Number(sanitized));
+  };
+
   return (
     <Input
       placeholder="1,000"
       mask={maskNumber}
       preffix={<CurrencyIcon />}
       type="number"
+      onChange={handleChange}
     />
   );
 }
